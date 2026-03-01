a852f617001ec65d22d73912ccdcd6d9e2447403
Asif9652 <rayadurgamasif@gmail.com>
2026-03-01 17:05:33 +0530

    thrid FoodieExpress project

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,8 +2,8 @@ const supabase = require('../config/supabase');
 const Razorpay = require('razorpay');
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+    key_id: process.env.RAZORPAY_KEY_ID || 'dummy_key_id',
+    key_secret: process.env.RAZORPAY_KEY_SECRET || 'dummy_key_secret',
 });
 
 exports.createOrder = async (req, res) => {
